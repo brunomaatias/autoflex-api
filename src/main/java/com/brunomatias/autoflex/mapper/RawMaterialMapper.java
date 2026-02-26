c399670ff18650ae99ae4f2a3b965342b392b29a
brunomaatias <bruno.gabriel0589@gmail.com>
2026-02-26 13:24:23 -0300

    feat: corrections on mapper raw materials.

--- src/main/java/com/brunomatias/autoflex/mapper/RawMaterialMapper.java
+++ src/main/java/com/brunomatias/autoflex/mapper/RawMaterialMapper.java
@@ -8,6 +8,7 @@ import com.brunomatias.autoflex.models.RawMaterial;
 public class RawMaterialMapper {
     public static RawMaterial toEntity(RawMaterialRequestDTO dto) {
         RawMaterial rawMaterial = new RawMaterial();
+        rawMaterial.setRawMaterialId(dto.rawMaterialId());
         rawMaterial.setCode(dto.code());
         rawMaterial.setName(dto.name());
         rawMaterial.setStockQuantity(dto.stockQuantity());
@@ -17,6 +18,7 @@ public class RawMaterialMapper {
 
     public static RawMaterialResponseDTO toResponseDTO(RawMaterial rawMaterial) {
         return new RawMaterialResponseDTO(
+                rawMaterial.getRawMaterialId(),
                 rawMaterial.getCode(),
                 rawMaterial.getName(),
                 rawMaterial.getStockQuantity()
