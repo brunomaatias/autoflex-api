51247e60efac9cbd2719fb3c12633c8e225d85b1
brunomaatias <bruno.gabriel0589@gmail.com>
2026-02-26 13:23:07 -0300

    feat: correction of a methot to return the suggest products with max production.

--- src/main/java/com/brunomatias/autoflex/repositories/ProductRawMaterialRepository.java
+++ src/main/java/com/brunomatias/autoflex/repositories/ProductRawMaterialRepository.java
@@ -2,6 +2,13 @@ package com.brunomatias.autoflex.repositories;
 
 import com.brunomatias.autoflex.models.ProductRawMaterial;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductRawMaterialRepository extends JpaRepository<ProductRawMaterial, Long> {
+
+    @Query("SELECT p FROM ProductRawMaterial p WHERE p.product.id = :id")
+    List<ProductRawMaterial> findByProductId(@Param("id") Long id);
 }
--- src/main/java/com/brunomatias/autoflex/services/ProductService.java
+++ src/main/java/com/brunomatias/autoflex/services/ProductService.java
@@ -2,19 +2,34 @@ package com.brunomatias.autoflex.services;
 
 import com.brunomatias.autoflex.dtos.ProductRequestDTO;
 import com.brunomatias.autoflex.dtos.ProductResponseDTO;
+import com.brunomatias.autoflex.dtos.ProductionSuggestionDTO;
 import com.brunomatias.autoflex.mapper.ProductMapper;
 import com.brunomatias.autoflex.models.Product;
+import com.brunomatias.autoflex.models.ProductRawMaterial;
+import com.brunomatias.autoflex.models.RawMaterial;
+import com.brunomatias.autoflex.repositories.ProductRawMaterialRepository;
 import com.brunomatias.autoflex.repositories.ProductRepository;
+import com.brunomatias.autoflex.repositories.RawMaterialRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private RawMaterialRepository rawMaterialRepository;
+
+    @Autowired
+    private ProductRawMaterialRepository productRawMaterialRepository;
 
     public List<ProductResponseDTO> findAll() {
         return productRepository.findAll()
@@ -23,8 +38,8 @@ public class ProductService {
                 .toList();
     }
 
-    public List<ProductResponseDTO> findAllByOrderByPriceDesc() {
-        return productRepository.findAllByOrderByPriceDesc()
+    public List<ProductResponseDTO> findAllByOrderByPrice() {
+        return productRepository.findAllByOrderByPrice()
                 .stream()
                 .map(ProductMapper::toResponseDTO)
                 .toList();
@@ -49,5 +64,52 @@ public class ProductService {
         Product updated = productRepository.save(existing);
 
         return ProductMapper.toResponseDTO(updated);
+    }
+
+    public List<ProductionSuggestionDTO> calculateProductionPlan() {
+        List<ProductionSuggestionDTO> result = new ArrayList<>();
+        List<Product> products = productRepository.findAllByOrderByPrice();
+
+        Map<Long, Integer> stockMap = rawMaterialRepository.findAll()
+                .stream()
+                .collect(Collectors.toMap(
+                        RawMaterial::getRawMaterialId,
+                        RawMaterial::getStockQuantity
+                ));
+
+        for (Product product : products) {
+            List<ProductRawMaterial> relations =
+                    productRawMaterialRepository.findByProductId(product.getProductId());
+
+            int maxProduction = Integer.MAX_VALUE;
+
+            for (ProductRawMaterial relation : relations) {
+                int available = stockMap.getOrDefault(
+                        relation.getRawMaterial().getRawMaterialId(),
+                        0
+                );
+                int possible = available / relation.getRequiredQuantity();
+
+                maxProduction = Math.min(maxProduction, possible);
+            }
+
+            if (maxProduction > 0) {
+                for (ProductRawMaterial relation : relations) {
+                    Long rawId = relation.getRawMaterial().getRawMaterialId();
+                    int used = relation.getRequiredQuantity() * maxProduction;
+
+                    stockMap.put(rawId, stockMap.get(rawId) - used);
+                }
+
+                result.add(new ProductionSuggestionDTO(
+                        product.getProductId(),
+                        product.getName(),
+                        maxProduction,
+                        product.getPrice(),
+                        product.getPrice().multiply(BigDecimal.valueOf(maxProduction))
+                ));
+            }
+        }
+        return result;
     }
 }
